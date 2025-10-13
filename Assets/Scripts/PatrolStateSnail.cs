0ce1739a6d13de0f66a63488092903e3ff9721c5
oliviascout <124936224+oliviascout@users.noreply.github.com>
2025-10-13 15:33:35 -0400

    added basis for snail enemy
    basic code, snail turns around at ledges and kinda bounce off each other

---
+++ Assets/Scripts/PatrolStateSnail.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PatrolStateSnail : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=hPBkbqqP4m0
+
+    public Rigidbody2D rb;
+    public Transform ledgeDetector;
+    public LayerMask groundLayer, enemyLayer;
+
+    
+    public float raycastDistance, enemyDistance;
+    public float speed;
+    private bool faceRight = true;
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(ledgeDetector.position, Vector2.down, raycastDistance, groundLayer);
+        RaycastHit2D hitEnemy = Physics2D.Raycast(ledgeDetector.position, Vector2.right, enemyDistance, enemyLayer);
+
+        if (hit.collider == null | hitEnemy.collider == true)
+        {
+            Rotate();
+            if(hitEnemy.collider == true)
+            {
+                Debug.Log("enemy");
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (faceRight)
+        {
+            rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(-speed, rb.linearVelocity.y);
+        }
+        
+    }
+
+    void Rotate()
+    {
+        faceRight = !faceRight;
+        transform.Rotate(0, 180, 0);
+        
+    }
+}
