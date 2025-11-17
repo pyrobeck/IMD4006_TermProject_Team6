8e99da71a53f5c3d252b31c6a902c2af78e5254f
Via <violetameyquade@cmail.carleton.ca>
2025-11-17 11:27:55 -0500

    who knows man

--- Assets/Scripts/cameraMovement.cs
+++ Assets/Scripts/cameraMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,9 +8,11 @@ public class cameraMovement : MonoBehaviour
     [SerializeField] Transform target;
     [SerializeField] PlayerController targetScript;
     float moveSpeedX = 20;
-    float moveSpeedY = 15;
+    [SerializeField] float moveSpeedY = 3;
     float standardMoveSpeedX = 20;
-    float standardMoveSpeedY = 15;
+    float standardMoveSpeedY = 3;
+    [SerializeField] float fallingMoveSpeedY = 20;
+    [SerializeField] float fastMoveSpeedY = 20;
     float directionFlippingMoveSpeed = 10;
 
 
@@ -17,6 +21,7 @@ public class cameraMovement : MonoBehaviour
     int direction = 1;
     float lookAheadOffsetX;
     Vector3 yOffset;
+    Vector3 naturalYOffset;
     Vector3 targetX;
     Vector3 targetY;
 
@@ -32,12 +37,17 @@ public class cameraMovement : MonoBehaviour
 
     float screenUpperLimit;
     float screenLowerLimit;
+    float topOfScreen;
+    float bottomOfScreen;
+    float screenLimitDistance;
 
     float deadzoneRight;
     float deadzoneLeft;
 
     float deadzoneRatio;
 
+    bool isFalling = false;
+    bool isAboveScren = false;
 
     void Start()
     {
@@ -45,27 +55,36 @@ public class cameraMovement : MonoBehaviour
         screenWidth = zoom * 3.6f;
         screenHeight = zoom * 2;
 
-        yOffset = new Vector3(0, (float)((screenHeight / 5)), 0);  //player will always be in the bottom bit of the screen
+        naturalYOffset = new Vector3(0, (float)((screenHeight / 5)), 0);  //player will always be in the bottom bit of the screen
+        yOffset = naturalYOffset;
 
         lookAheadOffsetX = (float)screenWidth / 6; //player will always have about 60% of the screen in front of them
 
-        screenUpperLimit = (screenHeight / 2 + yOffset.y);
+        // screenUpperLimit = (screenHeight / 2 + yOffset.y);
+        // screenLowerLimit = (-screenHeight / 2 + yOffset.y) + (screenHeight / 10);
+
+        screenUpperLimit = (screenHeight / 5 + yOffset.y);
         screenLowerLimit = (-screenHeight / 2 + yOffset.y) + (screenHeight / 10);
+        screenLimitDistance = MathF.Abs(screenLowerLimit - screenUpperLimit);
+        topOfScreen = (screenHeight / 2 + yOffset.y);
+        bottomOfScreen = (-screenHeight / 2 + yOffset.y);
 
         deadzoneRatio = screenWidth / 12;
         deadzoneRight = deadzoneRatio;
         deadzoneLeft = -deadzoneRatio;
+
+        SnapToTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
         //uncomment these if you need to see the screen limits for level design
-        //Debug.DrawRay(new Vector3(0, screenUpperLimit, 0), Vector3.right * 1000);
-        //Debug.DrawRay(new Vector3(0, screenLowerLimit, 0), Vector3.right * 1000);
+        Debug.DrawRay(new Vector3(0, screenUpperLimit, 0), Vector3.right * 1000);
+        Debug.DrawRay(new Vector3(0, screenLowerLimit, 0), Vector3.right * 1000);
         //and these for the camera movement deadzone
-        Debug.DrawRay(new Vector3(deadzoneRight, 0, 0), Vector3.up * 500);
-        Debug.DrawRay(new Vector3(deadzoneLeft, 0, 0), Vector3.up * 500);
+        //Debug.DrawRay(new Vector3(deadzoneRight, 0, 0), Vector3.up * 500);
+        //Debug.DrawRay(new Vector3(deadzoneLeft, 0, 0), Vector3.up * 500);
 
         updateDirection();
         updateLookAheadOffset();
@@ -143,6 +162,76 @@ public class cameraMovement : MonoBehaviour
 
     private void updateYOffset()
     {
+
+        if (target.position.y > screenUpperLimit)
+        {
+            screenUpperLimit = target.position.y;
+            screenLowerLimit = screenUpperLimit - screenLimitDistance;
+        }
+
+        if (target.position.y - 1 < screenLowerLimit)
+        {
+            screenLowerLimit = target.position.y - 1;
+            screenUpperLimit = screenLowerLimit + screenLimitDistance;
+        }
+        naturalYOffset.y = screenLowerLimit + screenHeight / 5;
+        if (targetScript.GetIsGrounded() == false)
+        {
+            return;
+        }
+
+        yOffset.y = Mathf.MoveTowards(yOffset.y, naturalYOffset.y + (screenHeight / 5), fastMoveSpeedY * Time.deltaTime);
+    }
+    private void updateYOffsetFailure()
+    {
+        Debug.Log("why the HELL are you running");
+
+        if (target.position.y > screenUpperLimit && targetScript.GetIsGrounded())
+        {
+            yOffset.y = Mathf.MoveTowards(yOffset.y, target.position.y + (screenHeight / 5), moveSpeedY * Time.deltaTime);
+        }
+
+        if (target.position.y < screenUpperLimit)
+        {
+            yOffset.y = Mathf.MoveTowards(yOffset.y, naturalYOffset.y, moveSpeedY * Time.deltaTime);
+        }
+
+        if (targetScript.GetLinearVelocityY() < 0 && target.position.y < screenLowerLimit)
+        {
+            moveSpeedY = fallingMoveSpeedY;
+            isFalling = true;
+        }
+
+
+    }
+
+    private void SetScreenBoundaries()
+    {
+        if (target.position.y > topOfScreen)
+        {
+            naturalYOffset.y = naturalYOffset.y + screenHeight;
+
+            topOfScreen = topOfScreen + screenHeight;
+            bottomOfScreen = bottomOfScreen + screenHeight;
+            screenUpperLimit = screenUpperLimit + screenHeight;
+            screenLowerLimit = screenLowerLimit + screenHeight;
+        }
+        if (target.position.y < bottomOfScreen)
+        {
+            naturalYOffset.y = naturalYOffset.y - screenHeight;
+            topOfScreen = topOfScreen - screenHeight;
+            bottomOfScreen = bottomOfScreen - screenHeight;
+            screenUpperLimit = screenUpperLimit - screenHeight;
+            screenLowerLimit = screenLowerLimit - screenHeight;
+        }
+    }
+    private void resetYOffset()
+    {
+        yOffset = naturalYOffset;
+    }
+    private void updateYOffsetOld()
+    {
+        Debug.Log("why the HELL are you running");
         //don't update camera vertically unless player is either grounded or falling
         if (targetScript.GetIsGrounded() == false && targetScript.GetLinearVelocityY() >= 0)
         {
@@ -193,9 +282,11 @@ public class cameraMovement : MonoBehaviour
     {
         updateDirection();
         updateLookAheadOffset();
-        updateYOffset();
+        resetYOffset();
+        UpdateXCameraPosition();
+        UpdateYCameraPosition();
 
-        Vector3 snapPos = new Vector3(target.position.x, target.position.y, 0) + lookAheadOffset + yOffset + zOffset;
+        Vector3 snapPos = new Vector3(targetX.x, targetY.y, zOffset.z);
 
         transform.position = snapPos;
     }
