1ae1cf800ce7d243fb12c70f04c2d39ec39f9943
Via <violetameyquade@cmail.carleton.ca>
2025-11-24 17:08:43 -0500

    Adjusted level 1

--- Assets/Scripts/Scripts-LevelDesign/LimitedZRotation.cs
+++ Assets/Scripts/Scripts-LevelDesign/LimitedZRotation.cs
@@ -8,18 +8,31 @@ public class AutoLimitedZRotation : MonoBehaviour
     public float maxRotation = -130f;
     public float rotationSpeed = 1f; // how fast it swings
 
+    private float rotationStart;
+    private int direction;
     private float range;
 
     void Start()
     {
         // Calculate total range
         range = maxRotation - minRotation;
+
+        if (rotationSpeed < 0)
+        {
+            rotationStart = maxRotation;
+            direction = -1;
+        }
+        else
+        {
+            rotationStart = minRotation;
+            direction = 1;
+        }
     }
 
     void Update()
     {
         // PingPong value oscillates between 0 and range
-        float angle = Mathf.PingPong(Time.time * rotationSpeed, range) + minRotation;
+        float angle = rotationStart + (direction * Mathf.PingPong(Time.time * rotationSpeed, range));
 
         // Apply to Z rotation
         transform.localEulerAngles = new Vector3(0f, 0f, angle);
